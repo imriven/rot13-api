48d3d3c0a6cb2df3a6a70852835f7c5338fb6512
Christina Harris <christinathedesigner@outlook.com>
2022-04-27 10:56:14 -0700

    server set up

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const cors = require("cors");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const server = express();
-const mathRouter = require("./router.js");
+const rotRouter = require("./rot13Router.js");
+const catFactsRouter = require("./catFactsRouter.js");
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use("/math", mathRouter);
+server.use("/api/rot13", rotRouter);
+server.use("/api/catFacts", catFactsRouter);
 
 module.exports = server;
